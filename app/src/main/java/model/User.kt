cf499736903dd5afd8b18e6b898d753548b5f70f
blackhegde <hoangkyanhyb1@gmail.com>
2025-01-18 22:21:00 +0700

    change user model

--- app/src/main/java/model/User.kt
+++ app/src/main/java/model/User.kt
@@ -3,7 +3,6 @@ package model
 import com.google.android.gms.common.util.Strings
 
 data class User (
-    val id : Int,
     val username : String,
     val email : Strings,
     val avartar_type : Int
